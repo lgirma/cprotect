9fd320741b1a362606624cab7d3688c6ab31ff07
Leone <leeman2016@gmail.com>
2022-06-02 14:15:31 +0300

    Use cmd to run wmic

--- machineid.go
+++ machineid.go
@@ -5,25 +5,39 @@ import (
 	"errors"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
-func getHDDSerialNumber() (string, error) {
-	wmicCmd := exec.Command("wmic", "diskdrive", "get", "serialnumber")
-	output, err := wmicCmd.Output()
+func queryWMIC(args ...string) (string, error) {
+	cmdPath := "cmd.exe"
+	cmdArgs := []string{"/c", "wmic"}
+	for _, arg := range args {
+		cmdArgs = append(cmdArgs, arg)
+	}
+	cmdInstance := exec.Command(cmdPath, cmdArgs...)
+	cmdInstance.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmdOutput, err := cmdInstance.Output()
 	if err != nil {
 		return "", err
 	}
-	sn := strings.Split(strings.TrimSuffix(string(output), "\n"), "\n")
+	return string(cmdOutput), nil
+}
+
+func getHDDSerialNumber() (string, error) {
+	output, err := queryWMIC("diskdrive", "get", "serialnumber") //exec.Command("wmic", "diskdrive", "get", "serialnumber")
+	if err != nil {
+		return "", err
+	}
+	sn := strings.Split(strings.TrimSuffix(output, "\n"), "\n")
 	return strings.TrimSpace(sn[1]), nil
 }
 
 func getBaseBoardSerialNumber() (string, error) {
-	wmicCmd := exec.Command("wmic", "baseboard", "get", "serialnumber")
-	output, err := wmicCmd.Output()
+	output, err := queryWMIC("baseboard", "get", "serialnumber") //exec.Command("wmic", "diskdrive", "get", "serialnumber")
 	if err != nil {
 		return "", err
 	}
-	sn := strings.Split(strings.TrimSuffix(string(output), "\n"), "\n")
+	sn := strings.Split(strings.TrimSuffix(output, "\n"), "\n")
 	return strings.TrimSpace(sn[1]), nil
 }
 
