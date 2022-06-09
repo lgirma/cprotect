05335d97fca4d2eb42f33f21c7ef5165693c0830
Leone <leeman2016@gmail.com>
2022-06-09 15:09:42 +0300

    Separate files for non-Windows platforms

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ build-activator:
 	cd cmd/activator; go build -ldflags "-s -w -X main.Product=Geezr-Go -X 'main.Password=$(CProtectPass)'" -o ../../bin/cprotect-activator.exe
 
 release:
-	git tag v0.2.0
-	git push origin HEAD
+	git tag v0.3.0
+	git push origin v0.3.0
---
+++ vault_not_pc.go
@@ -0,0 +1,17 @@
+//+build !(linux || mac || windows)
+
+package cprotect
+
+import "errors"
+
+func IsInstalled(product string, password string) (bool, error) {
+	return false, errors.New("NOT_IMPLEMENTED")
+}
+
+func IsActivationCodeValid(password string, requestCode string, activationCode string) bool {
+	return false
+}
+
+func Install(product string, activationCode string) error {
+	return errors.New("NOT_IMPLEMENTED")
+}
